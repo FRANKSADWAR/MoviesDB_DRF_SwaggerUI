f6e047c7cdc48699ee579162a73146ca574797c4
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-13 16:22:45 +0300

    neswe

--- movies/moviesdb/admin.py
+++ movies/moviesdb/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from moviesdb.models import Movies, Songs
 
-admin.site.register(Movies)
-admin.site.register(Songs)
-# Register your models here.
+
+
+@admin.register(Movies)
+class MoviesAdmin(admin.ModelAdmin):
+    list_display = ('name','staring','release_date','description','budget')
+    search_fields = ('name','budget','release_date','description','staring')
+    filter_fields = ('name','staring','description')
+
+
+@admin.register(Songs)
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ('name','artists','sold','date')
+    search_fields = ('name','artists','sold','date')
+    filter_fields = ('name','artists')
+
--- movies/moviesdb/models.py
+++ movies/moviesdb/models.py
@@ -7,7 +7,9 @@ class Movies(models.Model):
     release_date = models.DateField()
     description = models.CharField(max_length=200)
     staring = models.TextField(max_length=500)
-    
+
+    class Meta:
+        verbose_name_plural = 'Movies'
     def __str__(self):
         return '%s %s'.format(self.name, self.description)
 
@@ -16,6 +18,9 @@ class Songs(models.Model):
     artists = models.TextField(max_length=500)
     sold = models.IntegerField()
     date = models.DateField()
+
+    class Meta:
+        verbose_name_plural = 'Songs'
 
     def __str__(self):
         return '%s %s'.format(self.name, self.artists)
